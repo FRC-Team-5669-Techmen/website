16a71488f3c56788700da5528e14bb74eec75212
dazon <44048306+FakeDazon@users.noreply.github.com>
2021-09-23 16:18:35 -0700

    add images

--- scripts/main.js
+++ scripts/main.js
@@ -214,12 +214,14 @@ function load() {
     document.getElementById("nav-" + window.location.pathname.split("/")[2].replace('.html', '')).classList.add('active')
   }
 
-  if(q.get('[data-modal-img')) {
-    dynamicallyLoadScript("../scripts/modal.js")
-    dynamicallyLoadStyle("../styles/modal.css")
-    setTimeout(() => {
-      PicModal.init()
-    }, 100);
+  if (q.get('[data-modal-img')) {
+    dynamicallyLoadScript("../scripts/modal.js").then(() => {
+      dynamicallyLoadStyle("../styles/modal.css")
+      setTimeout(() => {
+        PicModal.init()
+      }, 100);
+
+    })
   }
 
 
@@ -380,18 +382,25 @@ function copy(text, options) {
 }
 
 
-function dynamicallyLoadScript(url) {
+async function dynamicallyLoadScript(url) {
   var script = document.createElement("script");  // create a script DOM node
   script.src = url;  // set its src to the provided URL
 
-  document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+  await document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
 
 function dynamicallyLoadStyle(url) {
   var link = document.createElement("link");  // create a script DOM node
   link.href = url;
-  link.rel  = 'stylesheet';
+  link.rel = 'stylesheet';
   link.type = 'text/css';  // set its src to the provided URL
 
   document.head.appendChild(link);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+function scroll(el) {
+    el.scrollIntoView({
+        behavior: 'smooth',
+        block: "center"
+    });
+    return false
 }
